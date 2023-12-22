9e7396ac69f90cf227152d373159eeebe8d7a688
Ray-Soong <lyyygz@163.com>
2023-12-22 23:16:06 +0000

    Add a webserver example

---
+++ webserver/webserver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace webserver
+{
+    class Program
+    {
+        static void Main()
+        {
+            var server = new SimpleWebServer("http://localhost:60000/");
+            server.Start();
+
+            Console.WriteLine("Press Enter to stop the server");
+            Console.ReadLine();
+
+            server.Stop();
+        }
+    }
+}
